cc1420e5f9339654f5bd921994ccee5d51bb2014
Luciano Candotti <lcandotti@gmail.com>
2025-09-04 11:24:51 -0300

    agregré consulta

---
+++ productos/producto.js
@@ -0,0 +1,42 @@
+let producto = 0
+const id = document.querySelector('.id')
+const btnAddAll = document.querySelectorAll('.btn-add')
+const add = document.getElementById('add')
+const addIcon = document.querySelector('#add .bi')
+const addTxt = document.getElementById('add-txt')
+
+const btnDesk = document.getElementById('btn-desk')
+const btnMovil = document.getElementById('btn-m')
+
+add.addEventListener('click', () => {
+    add.classList.toggle('btn-success')
+    add.classList.toggle('btn-danger')
+
+    btnAddAll.forEach(btn => {
+        btn.classList.toggle('d-none')
+        btn.classList.toggle('d-inline')
+    })
+
+    addIcon.classList.toggle('bi-check-lg')
+    addIcon.classList.toggle('bi-x-lg')
+
+    if (producto === 0) {
+        addTxt.textContent = 'Quitar de consultas'
+    } else {
+        addTxt.textContent = 'Agregar a consultas'
+    }
+    
+    // operador ternario => condición ? valor_si_true : valor_si_false
+    producto = producto === 0 ? 1 : 0
+    console.log(producto === 1 ? 'agregado' : 'quitado')
+})
+
+function wasap(e) {
+    e.preventDefault() // Si son enlaces
+        const mensaje = `Hola, quiero consultar presupuesto de '${id.textContent}'`
+        const url = `https://wa.me/+5493765252764?text=${encodeURIComponent(mensaje)}`
+        window.open(url, '_blank')
+}
+
+btnDesk.addEventListener('click', wasap)
+btnMovil.addEventListener('click', wasap)
